26ea07a59979a395d4cae1412cdca7befb3af7c7
Chillisoft <support@chillisoft.co.za>
2011-01-12 08:09:51 +0000

    updated rake file
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/Habanero.Testability/Trunk@14930 6b8f5710-b20c-734b-ac0a-461ee9249d7d

--- Rakefile.rb
+++ Rakefile.rb
@@ -38,58 +38,58 @@ msbuild :msdo_habanero do |msb| #builds habanero with msbuild
   
 #do_smooth tasks
   task :copy_dlls_to_smooth_lib  do #copies habanero DLLs to smooth lib
-	FileUtils.cp Dir.glob('temp/Habanero/trunk/bin/Habanero*.dll'), 'temp/Habanero Community/SmoothHabanero/trunk/lib'
+	FileUtils.cp Dir.glob('temp/Habanero/trunk/bin/Habanero*.dll'), 'temp/HabaneroCommunity/SmoothHabanero/trunk/lib'
 end
 
   task :clean_smooth do #deletes bin folder before build
-	FileUtils.rm_rf 'temp/Habanero Community/SmoothHabanero/trunk/bin'
+	FileUtils.rm_rf 'temp/HabaneroCommunity/SmoothHabanero/trunk/bin'
 end
 
 exec :checkout_smooth do |cmd| #command to check out smooth source using SVN
 	cmd.path_to_command = "../../../Utilities/BuildServer/Subversion/bin/svn.exe"
-	cmd.parameters %q(checkout "http://delicious:8080/svn/habanero/Habanero Community/SmoothHabanero/trunk/" "temp/Habanero Community/SmoothHabanero/trunk/" --username chilli --password chilli --non-interactive)
-	# %q(...) is used to encase the parameters in a quote, necessary  because of the space in 'Habanero Community'
+	cmd.parameters %q(checkout "http://delicious:8080/svn/habanero/HabaneroCommunity/SmoothHabanero/trunk/" "temp/HabaneroCommunity/SmoothHabanero/trunk/" --username chilli --password chilli --non-interactive)
+	# %q(...) is used to encase the parameters in a quote, necessary  because of the space in 'HabaneroCommunity'
 end
 
 msbuild :msdo_smooth do |msb| #builds smooth with msbuild
     msb.targets :Build
 	msb.path_to_command = "C:/Windows/Microsoft.NET/Framework64/v4.0.30319/MSBuild.exe"
 	msb.verbosity = "quiet"
-    msb.solution = "temp/Habanero Community/SmoothHabanero/trunk/source/SmoothHabanero_2010.sln"
+    msb.solution = "temp/HabaneroCommunity/SmoothHabanero/trunk/source/SmoothHabanero_2010.sln"
   end
   
   
 #do_faces tasks
   task :copy_dlls_to_faces_lib  do #copies habanero and smooth DLLs to faces lib
-	FileUtils.cp Dir.glob('temp/Habanero/trunk/bin/Habanero*.dll'), 'temp/Habanero Community/Faces/trunk/lib'
-	FileUtils.cp Dir.glob('temp/Habanero Community/SmoothHabanero/trunk/bin/Habanero.Smooth*.dll'), 'temp/Habanero Community/Faces/trunk/lib'
+	FileUtils.cp Dir.glob('temp/Habanero/trunk/bin/Habanero*.dll'), 'temp/HabaneroCommunity/Faces/trunk/lib'
+	FileUtils.cp Dir.glob('temp/HabaneroCommunity/SmoothHabanero/trunk/bin/Habanero.Smooth*.dll'), 'temp/HabaneroCommunity/Faces/trunk/lib'
 	end
   
     task :clean_faces do #deletes bin folder before build
-	FileUtils.rm_rf 'temp/Habanero Community/Faces/trunk/bin'
+	FileUtils.rm_rf 'temp/HabaneroCommunity/Faces/trunk/bin'
 end
 
 exec :checkout_faces do |cmd| #command to check out faces source using SVN
 	cmd.path_to_command = "../../../Utilities/BuildServer/Subversion/bin/svn.exe"
-	cmd.parameters %q(checkout "http://delicious:8080/svn/habanero/Habanero Community/Faces/trunk/" "temp/Habanero Community/Faces/trunk/" --username chilli --password chilli --non-interactive)
+	cmd.parameters %q(checkout "http://delicious:8080/svn/habanero/HabaneroCommunity/Faces/trunk/" "temp/HabaneroCommunity/Faces/trunk/" --username chilli --password chilli --non-interactive)
 end
 
 msbuild :msdo_faces do |msb| #builds faces with msbuild
     msb.targets :Build
 	msb.path_to_command = "C:/Windows/Microsoft.NET/Framework64/v4.0.30319/MSBuild.exe" #faces trunk at the time of the creation of this script was using .Net 4 features
 	msb.verbosity = "quiet"
-  msb.solution = "temp/Habanero Community/Faces/trunk/source/Habanero.Faces - 2010.sln"
+  msb.solution = "temp/HabaneroCommunity/Faces/trunk/source/Habanero.Faces - 2010.sln"
   end
   
   
 #do_testability tasks
   task :copy_dlls_to_testability_lib  do #copies habanero and smooth DLLs to testability lib
 	FileUtils.cp Dir.glob('temp/Habanero/trunk/bin/Habanero*.dll'), 'lib'
-	FileUtils.cp Dir.glob('temp/Habanero Community/SmoothHabanero/trunk/bin/Habanero.Smooth*.dll'), 'lib'
+	FileUtils.cp Dir.glob('temp/HabaneroCommunity/SmoothHabanero/trunk/bin/Habanero.Smooth*.dll'), 'lib'
 	end
   
     task :clean_testability do #deletes bin folder before build
-	FileUtils.rm_rf 'temp/Habanero Community/Habanero.Testability/Trunk/bin'
+	FileUtils.rm_rf 'temp/HabaneroCommunity/Habanero.Testability/Trunk/bin'
 end
 
 msbuild :msdo_testability do |msb| #builds testability with msbuild
