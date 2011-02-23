c31c40421d3e86363611fa94f0957cd5c66f580f
Peter Wiles <peter.wiles@chillisoft.co.za>
2011-02-23 15:10:20 +0000

    trying to fix Rakefile.rb
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/Habanero.Testability/trunk@17072 6b8f5710-b20c-734b-ac0a-461ee9249d7d

--- Rakefile.rb
+++ Rakefile.rb
@@ -9,9 +9,10 @@ require 'albacore'
 # deeper in the repo you will need to add another ..
 bs = File.dirname(__FILE__)
 bs = File.join(bs, "..") if bs.index("branches") != nil
-bs = File.join(bs, "../../HabaneroCommunity/BuildScripts")
-$:.unshift(File.expand_path(bs)) unless
-    $:.include?(bs) || $:.include?(File.expand_path(bs))
+bs = File.join(bs, "../../../HabaneroCommunity/BuildScripts")
+$buildscriptpath = File.expand_path(bs)
+$:.unshift($buildscriptpath) unless
+    $:.include?(bs) || $:.include?($buildscriptpath)
 
 #------------------------build settings--------------------------
 require 'rake-settings.rb'
